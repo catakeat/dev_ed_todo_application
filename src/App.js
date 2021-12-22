8b9f79ba394c192793ae0a34d146566adfe4fa03
catakeat <arsulescucatalin@yahoo.com>
2021-12-22 22:08:19 +0200

    iara testez2

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-    Test4
-=======
-    Test4
->>>>>>> 19b09cf650cb5fb5e819edf5a2c2dc1823176875
+
+    Test6
+
     </div>
   );
 }
